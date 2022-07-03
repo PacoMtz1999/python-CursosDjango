949fd7fe60014e6f1030fe3aeaa7880e31aaf05d
PacoMtz1999 <fran10martinez99@gmail.com>
2022-07-03 18:28:51 -0500

    Primer commit

---
+++ cursosDjango/contenido/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+menu=""" """
+
+def principal(request):
+    return render(request, "inicio/principal.html")
+
+def cursos(request):
+    return render(request, "inicio/cursos.html")
+
+def contacto(request):
+    return render(request, "inicio/contacto.html")
